19267b8531abaecb5dbe04d8a9ae998fbf072bfe
alzaburetz <anton.motin.dev@gmail.com>
2020-03-23 21:35:43 +0300

    Generic model for api response added

---
+++ Workflow/Workflow/Models/ResponseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Workflow.Models
+{
+    public class ResponseModel<T>
+    {
+        [JsonProperty("code")]
+        public int Code { get; set; }
+        [JsonProperty("errors")]
+        public List<string> Errors { get; set; }
+        [JsonProperty("response")]
+        public T Response { get; set; }
+    }
+}
