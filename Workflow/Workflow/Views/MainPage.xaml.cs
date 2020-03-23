17ad0aad28f0aa3b5adb5afbf5abcc3ac622f6b7
alzaburetz <anton.motin.dev@gmail.com>
2020-03-23 21:21:48 +0300

    initial files, start of the project

---
+++ Workflow/Workflow/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Workflow.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Xamarin.Forms.PlatformConfiguration.AndroidSpecific.TabbedPage.SetToolbarPlacement(this, Xamarin.Forms.PlatformConfiguration.AndroidSpecific.ToolbarPlacement.Bottom);
+        }
+        protected override void OnAppearing()
+        {
+            var token = Application.Current.Properties["Token"];
+            if (token == null)
+            {
+
+            }
+            base.OnAppearing();
+        }
+
+        protected override void OnCurrentPageChanged()
+        {
+            base.OnCurrentPageChanged();
+        }
+    }
+}
