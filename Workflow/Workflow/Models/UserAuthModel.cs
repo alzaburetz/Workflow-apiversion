9b6031aa9747ddb591db7463f85c19666a998448
alzaburetz <anton.motin.dev@gmail.com>
2020-03-23 22:32:05 +0300

    Model for user authorization added

---
+++ Workflow/Workflow/Models/UserAuthModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace Workflow.Models
+{
+    public class UserAuthModel
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+        [JsonProperty("phone")]
+        public string Phone { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
