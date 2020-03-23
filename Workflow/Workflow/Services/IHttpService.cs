c2964123eb75d21c4263d349aed857016d51c3d4
alzaburetz <anton.motin.dev@gmail.com>
2020-03-23 21:20:38 +0300

    HttpService interface added

---
+++ Workflow/Workflow/Services/IHttpService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Workflow.Services
+{
+    public interface IHttpService
+    {
+        HttpClient Client { get; set; }
+        string Token { get; set;}
+        Task<T> GetRequest<T>(string endpoint);
+        Task<T> PostRequest<T, D>(string endpoint, D data, bool token);
+        Task<T> PutRequest<T, D>(string endpoint, D data);
+        Task<T> DeleteRequest<T>(string endpoint);
+
+    }
+}
