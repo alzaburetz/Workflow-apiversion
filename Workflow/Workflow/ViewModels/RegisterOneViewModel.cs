e0a50b8b5111a395f4967a8ff425f31b9a070835
alzaburetz <anton.motin.dev@gmail.com>
2020-04-01 23:12:21 +0300

    Register request implemented

---
+++ Workflow/Workflow/ViewModels/RegisterOneViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using Xamarin.Forms;
+
+using Workflow.Models;
+
+namespace Workflow.ViewModels
+{
+    public class RegisterOneViewModel:BaseViewModel
+    {
+        public Command RegisterStepOne { get; set; }
+        public RegisterOneViewModel()
+        {
+            RegisterStepOne = new Command<UserAuthModel>(async (auth) =>
+            {
+                var resp = await HttpService.PostRequest<ResponseModel<UserModel>, UserAuthModel>("user/register", auth);
+                if (resp.Code != 200)
+                {
+                    await Application.Current.MainPage.DisplayAlert($"{resp.Code}", $"{resp.Errors[0]}", "OK");
+                }
+                else
+                {
+
+                }
+            });
+        }
+
+    }
+}
