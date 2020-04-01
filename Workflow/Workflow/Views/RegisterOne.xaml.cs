3c8d1cd818e704e0918c102f6e09ab63a91c50e0
alzaburetz <anton.motin.dev@gmail.com>
2020-04-01 23:16:34 +0300

    Page for registration is started

---
+++ Workflow/Workflow/Views/RegisterOne.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Workflow.ViewModels;
+using Workflow.Models;
+
+using System.Text.RegularExpressions;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterOne : ContentPage
+    {
+        public RegisterOne()
+        {
+            InitializeComponent();
+        }
+        public RegisterOne(RegisterOneViewModel viewModel):this()
+        {
+            BindingContext = viewModel;
+        }
+
+        protected async void Register(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(Number.Text) || 
+                string.IsNullOrEmpty(Email.Text) ||
+                string.IsNullOrEmpty(User.Text) ||
+                string.IsNullOrEmpty(Password.Text))
+            {
+                await Application.Current.MainPage.DisplayAlert("Ошибка", "Заполните все поля для продолжения!", "ОК");
+                return;
+            }
+            var phone = Number.Text;
+            var finalPhone = Regex.Replace(phone, @"\(", "");
+            finalPhone = Regex.Replace(finalPhone, @"\)", "");
+            finalPhone = Regex.Replace(finalPhone, @"\s+", "");
+            var user = new UserAuthModel()
+            {
+                Email = Email.Text,
+                Name = User.Text,
+                Password = Password.Text,
+                Phone = finalPhone
+            };
+            (BindingContext as RegisterOneViewModel).RegisterStepOne.Execute(user);
+        }
+    }
+}
