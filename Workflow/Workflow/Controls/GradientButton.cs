a937eecfa21802e04857ad057e807381072f2f28
alzaburetz <anton.motin.dev@gmail.com>
2020-04-01 23:10:38 +0300

    Custom Control - Gradient button

---
+++ Workflow/Workflow/Controls/GradientButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Controls
+{
+    public class GradientButton:Button
+    {
+        public BindableProperty StartColor { get; set; }
+        public BindableProperty EndColor { get; set; }
+        public Color Start { get; set; }
+        public Color End { get; set; }
+        public GradientButton() : base()
+        {
+            StartColor = BindableProperty.Create("StartColor", typeof(Color), typeof(Color), Color.FromHex("#2fa362"));
+            EndColor = BindableProperty.Create("EndColor", typeof(Color), typeof(Color), Color.FromHex("#237547"));
+            Start = (Color)GetValue(StartColor);
+            End = (Color)GetValue(EndColor);
+        }
+    }
+}
