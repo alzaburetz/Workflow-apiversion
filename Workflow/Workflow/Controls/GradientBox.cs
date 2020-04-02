3b29d9e0e2a96c7788f77a5b441c5a02b8ad4a84
alzaburetz <anton.motin.dev@gmail.com>
2020-04-02 18:46:23 +0300

    GradientBox added

---
+++ Workflow/Workflow/Controls/GradientBox.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Controls
+{
+    public class GradientBox:BoxView
+    {
+        public Color StartColor { get; set; }
+        public Color EndColor { get; set; }
+        public GradientBox()
+        {
+            if (StartColor == null)
+            {
+                StartColor = Color.FromHex("#d6d6d6");
+            }
+
+            if (EndColor == null)
+            {
+                EndColor = Color.White;
+            }
+        }
+    }
+}
