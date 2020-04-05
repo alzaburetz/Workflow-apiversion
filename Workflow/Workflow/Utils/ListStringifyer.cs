a2db82943d73299467a3388e491abdc2db33764a
alzaburetz <anton.motin.dev@gmail.com>
2020-04-05 20:46:14 +0300

    Utility to stingify list of strings

---
+++ Workflow/Workflow/Utils/ListStringifyer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Workflow.Utils
+{
+    public static class ListStringifyer
+    {
+        public static string StringifyList(List<string> list)
+        {
+            string result = string.Empty;
+            foreach (var str in list)
+            {
+                result += $"{str}, ";
+            }
+            return result.Trim().Remove(result.Length - 2, 1);
+        }
+    }
+}
