0aa148d9844787cf56dfb7e4eb96423998a3516f
alzaburetz <anton.motin.dev@gmail.com>
2020-04-05 20:56:12 +0300

    Login method added and implemented

--- Workflow/Workflow/Views/Login.xaml.cs
+++ Workflow/Workflow/Views/Login.xaml.cs
@@ -7,14 +7,20 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Workflow.Models;
+using Workflow.ViewModels;
+using System.Text.RegularExpressions;
+
 namespace Workflow.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        LoginViewModel viewModel { get; set; }
         public Login()
         {
             InitializeComponent();
+            BindingContext = viewModel = new LoginViewModel();
         }
 
         protected override void OnAppearing()
@@ -25,6 +31,26 @@ namespace Workflow.Views
         protected async void GoToRegistration(object sender, EventArgs args)
         {
             await Navigation.PushModalAsync(new RegisterOne(new ViewModels.RegisterOneViewModel()));
+        }
+
+        private async void ExecuteLogin(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(Number.Text))
+            {
+                await Application.Current.MainPage.DisplayAlert("Ошибка", "Заполните все поля для продолжения!", "ОК");
+                return;
+            }
+            var phone = Number.Text;
+            var finalPhone = Regex.Replace(phone, @"\(", "");
+            finalPhone = Regex.Replace(finalPhone, @"\)", "");
+            finalPhone = Regex.Replace(finalPhone, @"\s+", "");
+            finalPhone = finalPhone.Replace("+7", "8");
+            var user = new UserAuthModel()
+            {
+                Phone = finalPhone,
+                Password = Password.Text
+            };
+            viewModel.Login.Execute(user);
         }
     }
 }
