00ef5e50cb08e67272a8b047307dd090ac209151
alzaburetz <anton.motin.dev@gmail.com>
2020-04-05 20:57:30 +0300

    Create profile stage implemented

---
+++ Workflow/Workflow/ViewModels/CreateProfileViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using Workflow.Models;
+
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using Workflow.Utils;
+
+namespace Workflow.ViewModels
+{
+    public class CreateProfileViewModel:BaseViewModel
+    {
+        public UserModel User { get; set; }
+        public DateTime Date { get; set; }
+        public Command UpdateUser { get; set; }
+        public Command UploadPhoto { get; set; }
+        public MediaFile Avatar { get; set; }
+        public CreateProfileViewModel(UserModel user):this()
+        {
+            User = user;
+        }
+
+        public CreateProfileViewModel()
+        {
+            UpdateUser = new Command(async () =>
+            {
+                IsBusy = true;
+                var response = await HttpService.PutRequest<ResponseModel<UserModel>, UserModel>("user/update", User);
+                if (response.Code != 200)
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Application.Current.MainPage.DisplayAlert(response.Code.ToString(), ListStringifyer.StringifyList(response.Errors), "OK");
+                    });
+                    IsBusy = false;
+                    return;
+                }
+
+                if (Avatar != null)
+                {
+                    UploadPhoto.Execute(null);
+                }
+                else
+                {
+                    IsBusy = false;
+                    Navigate();
+                }
+            });
+
+            UploadPhoto = new Command(async () =>
+            {
+                var file = System.IO.File.ReadAllBytes(Avatar.AlbumPath);
+                var response = await HttpService.UploadFile<ResponseModel<string>>("user/avatar", file);
+                if (response.Code != 200)
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Application.Current.MainPage.DisplayAlert(response.Code.ToString(), ListStringifyer.StringifyList(response.Errors), "OK");
+                        IsBusy = false;
+                        return;
+                    });
+                }
+                Navigate();
+            });
+        }
+
+        private void Navigate()
+        {
+            Application.Current.MainPage = new Workflow.Views.MainPage();
+        }
+    }
+}
---
+++ Workflow/Workflow/Views/CreateProfile.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Workflow.ViewModels;
+using Plugin.Media.Abstractions;
+using Plugin.Media;
+using Plugin.Permissions;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CreateProfile : ContentPage
+    {
+        CreateProfileViewModel viewModel { get; set; }
+        public CreateProfile()
+        {
+            InitializeComponent();
+        }
+
+        public CreateProfile(CreateProfileViewModel ViewModel) : this()
+        {
+            this.viewModel = ViewModel;
+            BindingContext = this.viewModel;
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected async void TakePhoto(object sender, EventArgs args)
+        {
+            await CrossMedia.Current.Initialize();
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await Application.Current.MainPage.DisplayAlert("No Camera", ":( No camera available.", "OK");
+                return;
+            }
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            {
+                AllowCropping = true,
+                PhotoSize = PhotoSize.Medium
+            });
+            if (BindingContext == null)
+            {
+                BindingContext = viewModel = new CreateProfileViewModel();
+            }
+
+            if (photo != null)
+            {
+                viewModel.Avatar = photo;
+                Avatar.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = photo.GetStream();
+                    return stream;
+                });
+            }
+        }
+        private async void CreateProfilePressed(object sender, EventArgs args)
+        {
+            if (string.IsNullOrEmpty(Work.Text) || Work.Text.Length < 5)
+            {
+                await DisplayAlert(null, "Введите данные в правильном формате!", "ОК");
+                return;
+            }
+            var days = Work.Text.Split('/');
+            var dayone = days[0].TrimEnd();
+            var daytwo = days[1].TrimStart();
+            viewModel.User.Workdays = int.Parse(dayone);
+            viewModel.User.Weekdays = int.Parse(daytwo);
+            viewModel.User.FirstWork = (new DateTimeOffset(FirstDay.Date).ToUnixTimeSeconds()).ToString();
+            viewModel.User.Surname = Surname.Text;
+
+            viewModel.UpdateUser.Execute(null);
+        }
+    }
+}
