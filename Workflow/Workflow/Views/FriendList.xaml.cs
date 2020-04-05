99ea3baa8f9e6b3e73529d62868f457bf67bbac7
alzaburetz <anton.motin.dev@gmail.com>
2020-04-05 20:58:24 +0300

    Request for reading contacts

--- Workflow/Workflow/Views/FriendList.xaml.cs
+++ Workflow/Workflow/Views/FriendList.xaml.cs
@@ -4,17 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Workflow.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using Plugin.Permissions;
+using Plugin.Permissions.Abstractions;
 
 namespace Workflow.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FriendList : ContentPage
     {
+        FriendListViewModel viewModel { get; set; }
         public FriendList()
         {
             InitializeComponent();
+            BindingContext = viewModel = new FriendListViewModel();
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            await CrossPermissions.Current.RequestPermissionsAsync(Permission.Contacts).ContinueWith((obj) =>
+            {
+                viewModel.GetContacts.Execute(null);
+            });
         }
     }
 }
