327861055472d082d00c462a46d639a18d4fc6d2
alzaburetz <anton.motin.dev@gmail.com>
2020-04-05 20:47:23 +0300

    Custom datepicker renderer implemented

---
+++ Workflow/Workflow.Android/Renderers/CustomDatePickerRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Workflow.Controls;
+using Workflow.Droid.Renderers;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms;
+
+[assembly:ExportRenderer(typeof(CustomDatePicker),typeof(CustomDatePickerRenderer))]
+namespace Workflow.Droid.Renderers
+{
+    public class CustomDatePickerRenderer:DatePickerRenderer
+    {
+        public CustomDatePickerRenderer(Context context):base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.DatePicker> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                this.Control.SetBackgroundColor(Xamarin.Forms.Color.Transparent.ToAndroid());
+                this.Control.SetHintTextColor(Xamarin.Forms.Color.White.ToAndroid());
+                this.Control.SetTextColor(Xamarin.Forms.Color.White.ToAndroid());
+            }
+        }
+    }
+}
