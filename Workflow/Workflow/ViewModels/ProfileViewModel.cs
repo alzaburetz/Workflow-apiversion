0ac43b8a0cb0af8d5d7cd67ca139b1b6e40d12e6
alzaburetz <anton.motin.dev@gmail.com>
2020-04-06 22:06:07 +0300

    Profile page added

---
+++ Workflow/Workflow/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Workflow.Models;
+using Xamarin.Forms;
+
+namespace Workflow.ViewModels
+{
+    public class ProfileViewModel:BaseViewModel
+    {
+        public Command GetUserData { get; set; }
+        private UserModel user_;
+        public UserModel User
+        {
+            get => user_;
+            set
+            {
+                user_ = value;
+                OnPropertyChanged("User");
+            }
+        }
+        public ProfileViewModel()
+        {
+            Title = "Профиль";
+            GetUserData = new Command(async () =>
+            {
+                var resp = await HttpService.GetRequest<ResponseModel<UserModel>>("user?search=89157508874");
+                User = resp.Response;
+            });
+        }
+    }
+}
---
+++ Workflow/Workflow/Views/Profile.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Workflow.ViewModels;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Profile : ContentPage
+    {
+        ProfileViewModel viewModel { get; set; }
+        public Profile()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new ProfileViewModel();
+            MessagingCenter.Subscribe<MainPage>(this, "LoadProfile", (obj) =>
+            {
+                viewModel.GetUserData.Execute(null);
+            });
+        }
+    }
+}
