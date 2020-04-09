e6e4a50d08b1d69696ad7b055e25e10ce904213f
alzaburetz <anton.motin.dev@gmail.com>
2020-04-09 21:58:53 +0300

    Calendar model added

---
+++ Workflow/Workflow/Models/CalendarModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Workflow.ViewModels;
+
+namespace Workflow.Models
+{
+    public class CalendarModel:BaseViewModel
+    {
+        public int DayOfMonth { get; set; }
+        public bool Workday { get; set; }
+        public int DayOfWeek { get; set; }
+        public bool IsThisMonth { get; set; }
+    }
+}
