f5dd6e64452ffa37219a6909b795b9ba5d28208f
alzaburetz <anton.motin.dev@gmail.com>
2020-04-09 22:00:07 +0300

    Calendar page added

---
+++ Workflow/Workflow/Views/CalendarPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Workflow.ViewModels;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalendarPage : ContentPage
+    {
+        CalendarViewModel viewModel { get; set; }
+        public CalendarPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new CalendarViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.CalculateCalendar.Execute(null);
+        }
+    }
+}
