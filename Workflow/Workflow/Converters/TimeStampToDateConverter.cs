e4bf2fcd0ef2fcf3c7146a4621a09dcbd0378220
alzaburetz <anton.motin.dev@gmail.com>
2020-04-09 21:58:43 +0300

    Converter from timestamp to date added

---
+++ Workflow/Workflow/Converters/TimeStampToDateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace Workflow.Converters
+{
+    public class TimeStampToDateConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            long timestamp = 0;
+            long.TryParse(value.ToString(), out timestamp);
+            if (timestamp > 0)
+            {
+                return DateTimeOffset.FromUnixTimeSeconds(timestamp).ToString("dd.MM.yyyy");
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
