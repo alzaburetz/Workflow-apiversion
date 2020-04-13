bc34921dc48fd6db08769ca480115d390665e081
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-13 22:18:53 +0300

    Profile edit added with logic

---
+++ Workflow/Workflow/ViewModels/ProfileEditViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using Workflow.Models;
+using Workflow.Utils;
+
+namespace Workflow.ViewModels
+{
+    public class ProfileEditViewModel:BaseViewModel
+    {
+        private UserModel user_;
+        public UserModel User
+        {
+            get => user_;
+            set
+            {
+                user_ = value;
+                OnPropertyChanged("User");
+            }
+        }
+        public Command UpdateUser { get; set; }
+        INavigation Navigation { get; set; }
+        public ProfileEditViewModel(UserModel User, INavigation nav)
+        {
+            this.Navigation = nav;
+            this.User = User;
+            UpdateUser = new Command<UserModel>(async (user) =>
+            {
+                IsBusy = true;
+                var resp = await HttpService.PutRequest<ResponseModel<UserModel>, UserModel>("user/update", user);
+                IsBusy = false;
+                if (resp.Code == 200)
+                {
+                    await Navigation.PopAsync();
+                } 
+                else
+                {
+                    await Application.Current.MainPage.DisplayAlert("Произошла ошибка", Utils.ListStringifyer.StringifyList(resp.Errors), "OK");
+                }
+            });
+        }
+    }
+}
---
+++ Workflow/Workflow/Views/ProfileEdit.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Workflow.ViewModels;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProfileEdit : ContentPage
+    {
+        ProfileEditViewModel viewModel { get; set; }
+        public ProfileEdit()
+        {
+            InitializeComponent();
+            Datepicker.DateSelected += (s, e) =>
+            {
+                viewModel.User.NextWorkDay = e.NewDate;
+                viewModel.User.FirstWork = new DateTimeOffset(e.NewDate).ToUnixTimeSeconds().ToString();
+            };
+        }
+
+        public ProfileEdit(ProfileEditViewModel viewmodel) : this()
+        {
+            BindingContext = viewModel = viewmodel;
+        }
+
+        private void EditProfile(object sender, EventArgs args)
+        {
+            if (this.Schedule.Text.Length > 4)
+            {
+                var schedule = Schedule.Text.Split('/');
+                this.viewModel.User.Workdays = int.Parse(schedule[0].Trim());
+                this.viewModel.User.Weekdays = int.Parse(schedule[1].Trim());
+
+                viewModel.UpdateUser.Execute(this.viewModel.User);
+            }
+            else
+            {
+                DisplayAlert(null, "Введите правильные данные!", "OK");
+            }
+        }
+    }
+}
