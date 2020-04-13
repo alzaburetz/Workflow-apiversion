eaeceb30dc4db11039809f1d28bff8b747e31879
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-13 20:44:30 +0300

    Converter for text color in calender added

---
+++ Workflow/Workflow/Converters/ThisMonthDayColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Converters
+{
+    public class ThisMonthDayColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var thisMonth = (bool)value;
+            if (thisMonth)
+            {
+                return Color.White;
+            }
+            return Color.FromHex("#A3A3A3");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
