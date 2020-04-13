a4d34dbb5716a09367b2d584acf135eb0391c22a
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-13 20:44:55 +0300

    Color converter for workday added

---
+++ Workflow/Workflow/Converters/WorkDayColorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Converters
+{
+    public class WorkDayColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isWorkingDay = (bool)value;
+            if (isWorkingDay)
+            {
+                return Color.FromHex("#fa5252");
+            }
+            return Color.FromHex("#20a830");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
