ba63475710ebe58bb9ceca6850e8ed33802ebe09
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-18 22:16:43 +0300

    Method for uploading file

--- Workflow/Workflow/ViewModels/ProfileEditViewModel.cs
+++ Workflow/Workflow/ViewModels/ProfileEditViewModel.cs
@@ -5,11 +5,16 @@ using System.Text;
 using Xamarin.Forms;
 using Workflow.Models;
 using Workflow.Utils;
+using Workflow.Views;
+
+using Plugin.Media.Abstractions;
+using Plugin.Media;
 
 namespace Workflow.ViewModels
 {
     public class ProfileEditViewModel:BaseViewModel
     {
+        public MediaFile Avatar;
         private UserModel user_;
         public UserModel User
         {
@@ -21,6 +26,7 @@ namespace Workflow.ViewModels
             }
         }
         public Command UpdateUser { get; set; }
+        public Command UploadFile { get; set; }
         INavigation Navigation { get; set; }
         public ProfileEditViewModel(UserModel User, INavigation nav)
         {
@@ -30,15 +36,42 @@ namespace Workflow.ViewModels
             {
                 IsBusy = true;
                 var resp = await HttpService.PutRequest<ResponseModel<UserModel>, UserModel>("user/update", user);
-                IsBusy = false;
                 if (resp.Code == 200)
                 {
-                    await Navigation.PopAsync();
+                    if (Avatar != null)
+                    {
+                        UploadFile.Execute(null);
+                    }
+                    else
+                    {
+                        await Navigation.PopAsync();
+                    }
                 } 
                 else
                 {
                     await Application.Current.MainPage.DisplayAlert("Произошла ошибка", Utils.ListStringifyer.StringifyList(resp.Errors), "OK");
                 }
+                IsBusy = false;
+            });
+
+            UploadFile = new Command(async () =>
+            {
+                var file = System.IO.File.ReadAllBytes(Avatar.AlbumPath);
+                var response = await HttpService.UploadFile<ResponseModel<string>>("user/avatar", file);
+                if (response.Code != 200)
+                {
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Application.Current.MainPage.DisplayAlert(response.Code.ToString(), ListStringifyer.StringifyList(response.Errors), "OK");
+                        IsBusy = false;
+                        return;
+                    });
+                }
+                else
+                {
+                    await Navigation.PopAsync();
+                }
+                IsBusy = false;
             });
         }
     }
