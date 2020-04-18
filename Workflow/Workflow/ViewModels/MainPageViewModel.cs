1d37e27ce4734a4e70a3e05e77e7eb8d68330021
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-18 22:16:11 +0300

    ViewModel has command that checks if user data is full, if not, MainPage is CreateProfile

---
+++ Workflow/Workflow/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using Workflow.Models;
+using Workflow.Views;
+
+namespace Workflow.ViewModels
+{
+    public class MainPageViewModel:BaseViewModel
+    {
+        UserModel User { get; set; }
+        public Command CheckUser { get; set; }
+        public MainPageViewModel()
+        {
+            CheckUser = new Command(async () =>
+            {
+                var resp = await HttpService.GetRequest<ResponseModel<UserModel>>("user");
+                if (resp.Response.Workdays == 0)
+                {
+                    Application.Current.MainPage = new CreateProfile(new CreateProfileViewModel(resp.Response));
+                }
+            });
+        }
+    }
+}
