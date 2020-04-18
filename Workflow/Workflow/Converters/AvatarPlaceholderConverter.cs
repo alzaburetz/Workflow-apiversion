7637157a12eb8d7d0e81a00d92a559f25c04b177
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-18 22:14:11 +0300

    Converter for avatar Image source

---
+++ Workflow/Workflow/Converters/AvatarPlaceholderConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Converters
+{
+    public class AvatarPlaceholderConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string path = (string)value;
+            if (path == "")
+            {
+                return "avatar_placeholder";
+            }
+            else
+            {
+                return path;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
