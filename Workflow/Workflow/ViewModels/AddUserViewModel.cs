a59c6bebb6f7b079f11b8f7d89fe2f1eb8eba9b4
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-19 23:04:38 +0300

    Add user page with full functionality added

---
+++ Workflow/Workflow/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+using Workflow.Models;
+using Workflow.Utils;
+
+namespace Workflow.ViewModels
+{
+    public class AddUserViewModel:BaseViewModel
+    {
+        public UserModel NewUser { get; set; }
+        readonly INavigation Navigation;
+        public Command AddNewUserCommand { get; set; }
+        DateTime date;
+        public DateTime Date
+        {
+            get => date;
+            set
+            {
+                date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+
+        public AddUserViewModel(INavigation navigation)
+        {
+            Navigation = navigation;
+            Date = DateTime.Now;
+            AddNewUserCommand = new Command<UserModel>(async (user) =>
+            {
+                IsBusy = true;
+                if (user != null)
+                {
+                    if (user.Name.Length == 0 || user.Surname.Length == 0)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Заполните данные", "Введите имя и фамилию для добавления пользователя", "ОК");
+                        return;
+                    }
+                    if (user.Workdays == 0 || user.Weekdays == 0)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Заполните данные", "Введите корректный график для добавления пользователя", "ОК");
+                        return;
+                    }
+
+                    if (string.IsNullOrEmpty(user.Email))
+                    {
+                        user.Email = "temp@email";
+                    }
+                    if (user.Phone.Length == 0)
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Заполните данные", "Введите телефон для добавления пользователя", "ОК");
+                        return;
+                    }
+
+                    var resp = await HttpService.PostRequest<ResponseModel<UserModel>, UserModel>("user/add", user, true);
+                    if (resp.Code == 200)
+                    {
+                        await Navigation.PopAsync();
+                    }
+                    else
+                    {
+                        await Application.Current.MainPage.DisplayAlert("Произошла ошибка", ListStringifyer.StringifyList(resp.Errors), "ОК");
+                        return;
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Workflow/Workflow/Views/AddUser.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Workflow.Models;
+using Workflow.ViewModels;
+using System.Text.RegularExpressions;
+
+namespace Workflow.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddUser : ContentPage
+    {
+        AddUserViewModel viewModel { get; set; }
+        public AddUser()
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new AddUserViewModel(this.Navigation);
+        }
+
+        void AddUserButton(object sender, EventArgs args)
+        {
+            if (FullName.Text.Length == 0 || 
+                !FullName.Text.Contains(" ") || 
+                Schedule.Text.Length < 5 || 
+                Phone.Text.Length < 18)
+            {
+                DisplayAlert(null, "Для продолжения введите корректные данные!", "ОК");
+                return;
+            }
+            var user = new UserModel();
+            var names = FullName.Text.Split(' ');
+            user.Name = names[0];
+            user.Surname = names[1];
+            var schedule = Schedule.Text.Split('/');
+            user.Workdays = int.Parse(schedule[0].Trim());
+            user.Weekdays = int.Parse(schedule[1].Trim());
+            user.Phone = Regex.Replace(Phone.Text.Replace("+7", "8"), @"\D", "");
+            user.Email = Email.Text;
+            user.FirstWork = (new DateTimeOffset(FirstDay.Date).ToUnixTimeSeconds());
+            viewModel.AddNewUserCommand.Execute(user);
+        }
+    }
+}
