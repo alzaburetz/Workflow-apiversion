eb7b8ec77b6cb1bbee4c25ec16e56e8745678a38
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-21 23:45:20 +0300

    User can only add people from contacts list

--- Workflow/Workflow/Views/AddUser.xaml.cs
+++ Workflow/Workflow/Views/AddUser.xaml.cs
@@ -11,6 +11,9 @@ using Workflow.Models;
 using Workflow.ViewModels;
 using System.Text.RegularExpressions;
 
+using Plugin.ContactService;
+using Plugin.ContactService.Shared;
+
 namespace Workflow.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -25,9 +28,9 @@ namespace Workflow.Views
 
         void AddUserButton(object sender, EventArgs args)
         {
-            if (FullName.Text.Length == 0 || 
-                !FullName.Text.Contains(" ") || 
-                Schedule.Text.Length < 5 || 
+            if (FullName.Text.Length == 0 ||
+                !FullName.Text.Contains(" ") ||
+                Schedule.Text.Length < 5 ||
                 Phone.Text.Length < 18)
             {
                 DisplayAlert(null, "Для продолжения введите корректные данные!", "ОК");
@@ -44,6 +47,49 @@ namespace Workflow.Views
             user.Email = Email.Text;
             user.FirstWork = (new DateTimeOffset(FirstDay.Date).ToUnixTimeSeconds());
             viewModel.AddNewUserCommand.Execute(user);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            viewModel.LoadContactsCommand.Execute(null);
+        }
+
+        private void Phone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue.Length > e.OldTextValue?.Length)
+            {
+                viewModel.FilterContacts.Execute(Regex.Replace(e.NewTextValue.Replace("+7", "8"), @"\D", ""));
+            }
+            else
+            {
+                viewModel.Searching = false;
+            }
+        }
+
+        private void SelectContats(object sender, SelectionChangedEventArgs args)
+        {
+            try
+            {
+                var contact = (sender as CollectionView).SelectedItem as Contact;
+                FullName.Text = contact.Name;
+                var number_input = Regex.Replace(Phone.Text.Replace("+7", "8"), @"\D", "");
+                var number = contact.Numbers.Find(x => x.Contains(number_input));
+                StringBuilder builder = new StringBuilder();
+                builder.Append("+7 ")
+                       .Append($"({number.Substring(1, 3)}) ")
+                       .Append($"{number.Substring(4, 3)} ")
+                       .Append($"{number.Substring(7, 2)} ")
+                       .Append($"{number.Substring(9, 2)}");
+                Phone.Text = builder.ToString();
+                Email.Text = contact.Email;
+                viewModel.Searching = false;
+                (sender as CollectionView).SelectedItem = null;
+            }
+            catch
+            {
+
+            }
         }
     }
 }
