cb3b68c55a8803a805a042f8db7537bc853d5cea
Антон Мотин <anton.motin.dev@gmail.com>
2020-04-25 11:13:06 +0300

    Interface for setting up status bar color added

---
+++ Workflow/Workflow/Services/ISetStatusBarColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Workflow.Services
+{
+    public interface ISetStatusBarColor
+    {
+        void SetStatusBarColor(Color color);
+    }
+}
