1bed921f6e6b540eb74c7c3039071fb59a9cb096
Anton Motin <anton.motin.dev@gmail.com>
2020-04-30 23:53:52 +0300

    Custom control for editor added

---
+++ Workflow/Workflow/Controls/CustomEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Controls
+{
+    public class CustomEditor:Editor
+    {
+        public int CornerRadius { get; set; }
+        public int Padding { get; set; }
+        public Color Background { get; set; }
+        public CustomEditor()
+        {
+            if (Background == null)
+            {
+                Background = Color.White;
+            }
+        }
+    }
+}
