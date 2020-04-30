fb5136f28086c4b4fe46af82eaeaa1085dc25237
Anton Motin <anton.motin.dev@gmail.com>
2020-04-30 23:54:11 +0300

    PropertyChanged for Workday field

--- Workflow/Workflow/Models/CalendarModel.cs
+++ Workflow/Workflow/Models/CalendarModel.cs
@@ -9,7 +9,16 @@ namespace Workflow.Models
     public class CalendarModel:BaseViewModel
     {
         public int DayOfMonth { get; set; }
-        public bool Workday { get; set; }
+        bool _workday;
+        public bool Workday
+        {
+            get => _workday;
+            set
+            {
+                _workday = value;
+                OnPropertyChanged("Workday");
+            }
+        }
         public int DayOfWeek { get; set; }
         public bool IsThisMonth { get; set; }
         public int NumberOfWeek { get; set; }
