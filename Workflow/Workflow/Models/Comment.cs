5498f162a184b28b557cef56afe02b9f21e45646
Anton Motin <anton.motin.dev@gmail.com>
2020-04-30 23:54:20 +0300

    Comment model added

---
+++ Workflow/Workflow/Models/Comment.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+using Newtonsoft.Json;
+
+
+namespace Workflow.Models
+{
+    [Serializable]
+    public class Comment
+    {
+        [JsonProperty("id")]
+        public string ID { get; set; }
+        [JsonProperty("postid")]
+        public string PostID { get; set; }
+        [JsonProperty("author")]
+        public UserModel Author { get; set; }
+        [JsonProperty("at")]
+        public UserModel At { get; set; }
+        [JsonProperty("body")]
+        public string Body { get; set; }
+        [JsonProperty("created")]
+        public long Created { get; set; }
+        [JsonProperty("edited")]
+        public long Edited { get; set; }
+        [JsonProperty("likes")]
+        public int Likes { get; set; }
+    }
+}
