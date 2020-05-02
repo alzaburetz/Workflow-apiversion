14a611781dacd3e6ccd2846b57e48e5cbe78f133
Anton Motin <anton.motin.dev@gmail.com>
2020-05-02 20:48:42 +0300

    Property changed implementation added

---
+++ Workflow/Workflow/Models/ModelChanged.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace Workflow.Models
+{
+    public class ModelChanged:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName = "")
+        {
+            var changed = PropertyChanged;
+            if (changed == null)
+                return;
+
+            changed.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
