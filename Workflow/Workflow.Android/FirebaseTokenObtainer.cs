f75447bfb9e3f8c0f632d89553e88c983e1f43f2
Anton Motin <anton.motin.dev@gmail.com>
2020-05-02 20:46:07 +0300

    Method for obtaining token added

---
+++ Workflow/Workflow.Android/FirebaseTokenObtainer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Workflow.Services;
+using Xamarin.Forms;
+
+using Workflow.Droid;
+
+using Firebase.Iid;
+
+[assembly:Dependency(typeof(FirebaseTokenObtainer))]
+namespace Workflow.Droid
+{
+    public class FirebaseTokenObtainer : IFirebaseTokenObtainer
+    {
+        public string GetFirebaseToken()
+        {
+            return Firebase.Iid.FirebaseInstanceId.Instance.Token;
+        }
+    }
+}
