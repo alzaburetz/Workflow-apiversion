a13a06d3ec5abe041fc7622663b05b40b6bf2e8a
Anton Motin <anton.motin.dev@gmail.com>
2020-05-02 20:48:21 +0300

    Calendar model used in user model

--- Workflow/Workflow/Models/CalendarModel.cs
+++ Workflow/Workflow/Models/CalendarModel.cs
@@ -4,12 +4,16 @@ using System.Text;
 
 using Workflow.ViewModels;
 
+using Newtonsoft.Json;
+
 namespace Workflow.Models
 {
     public class CalendarModel:BaseViewModel
     {
+        [JsonProperty("day")]
         public int DayOfMonth { get; set; }
         bool _workday;
+        [JsonProperty("works")]
         public bool Workday
         {
             get => _workday;
@@ -19,7 +23,12 @@ namespace Workflow.Models
                 OnPropertyChanged("Workday");
             }
         }
+        [JsonProperty("day_of_week")]
         public int DayOfWeek { get; set; }
+        [JsonProperty("month")]
+        public int Month { get; set; }
+        [JsonProperty("note")]
+        public string Note { get; set; }
         public bool IsThisMonth { get; set; }
         public int NumberOfWeek { get; set; }
     }
