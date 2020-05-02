71d132c5754bae45538857fa71ca6ef609c15b7b
Anton Motin <anton.motin.dev@gmail.com>
2020-05-02 20:50:53 +0300

    Interface for getting firebase token

---
+++ Workflow/Workflow/Services/IFirebaseTokenObtainer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Services
+{
+    public interface IFirebaseTokenObtainer
+    {
+        string GetFirebaseToken();
+    }
+}
