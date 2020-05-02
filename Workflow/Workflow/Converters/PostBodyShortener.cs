1d7219cba5fe1956ed37a45ed8eea116b6baf90e
Anton Motin <anton.motin.dev@gmail.com>
2020-05-02 20:47:56 +0300

    Converter shortens post body if over 150 characters

---
+++ Workflow/Workflow/Converters/PostBodyShortener.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Workflow.Converters
+{
+    public class PostBodyShortener : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().Length > 100)
+            {
+                return (value.ToString().Remove(100) + "...");
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
