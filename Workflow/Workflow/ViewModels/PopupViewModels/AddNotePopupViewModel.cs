90ee46501911793c46c667bcf77fe7d3a8a6b664
Anton Motin <anton.motin.dev@gmail.com>
2020-05-09 23:57:05 +0300

    PopUp for adding note added

---
+++ Workflow/Workflow/ViewModels/PopupViewModels/AddNotePopupViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Workflow.Models;
+using Workflow.ViewModels;
+using Xamarin.Forms;
+
+namespace Workflow.ViewModels.PopupViewModels
+{
+    public class AddNotePopupViewModel:BaseViewModel
+    {
+        CalendarModel _day;
+        public CalendarModel Day
+        {
+            get => _day;
+            set
+            {
+                _day = value;
+                OnPropertyChanged("Day");
+            }
+        }
+        public string PreviousNote { get; set; }
+
+        public Command AddNote { get; set; }
+        public Command ClearNote { get; set; }
+        readonly INavigation Navigation;
+        long _date;
+        public long Date
+        {
+            get => _date;
+            set
+            {
+                _date = value;
+                OnPropertyChanged("Date");
+            }
+        }
+        public AddNotePopupViewModel(CalendarModel day, INavigation navigation)
+        {
+            Day = day;
+            Date = (new DateTimeOffset(new DateTime(DateTime.Now.Year, day.Month, day.DayOfMonth))).ToUnixTimeSeconds();
+            PreviousNote = day.Note;
+            Navigation = navigation;
+            AddNote = new Command<string>(async (note) =>
+            {
+                Day.Note = note;
+                await Navigation.PopPopupAsync();
+            });
+            ClearNote = new Command(async () =>
+            {
+                Day.Note = PreviousNote;
+                await Navigation.PopPopupAsync();
+            });
+        }
+    }
+}
---
+++ Workflow/Workflow/Views/Popups/AddNotePopup.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Extensions;
+
+using Workflow.ViewModels.PopupViewModels;
+
+namespace Workflow.Views.Popups
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddNotePopup : PopupPage
+    {
+        AddNotePopupViewModel viewModel { get; set; }
+        public AddNotePopup(AddNotePopupViewModel vm)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = vm;
+        }
+
+        protected override bool OnBackgroundClicked()
+        {
+            return false;
+
+        }
+    }
+}
