ab1f11c16fd495f3612101c9d99cc73cb8db4069
Anton Motin <anton.motin.dev@gmail.com>
2020-05-09 18:29:11 +0300

    Command for updating calendar is created

--- Workflow/Workflow/ViewModels/CalendarEditViewModel.cs
+++ Workflow/Workflow/ViewModels/CalendarEditViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Dynamic;
 using System.Text;
-
+using System.Threading.Tasks;
 using Workflow.Models;
 using Xamarin.Forms;
 
@@ -12,17 +12,12 @@ namespace Workflow.ViewModels
     public class CalendarEditViewModel:BaseViewModel
     {
         public Command InvertDay { get; set; }
-        ObservableCollection<CalendarModel> _calendarList;
+        public Command LoadCalendar { get; set; }
+        public Command UpdateCalendar { get; set; }
+        readonly INavigation Navigation;
         string _month;
-        public ObservableCollection<CalendarModel> CalendarList
-        {
-            get => _calendarList;
-            set
-            {
-                _calendarList = value;
-                OnPropertyChanged("CalendarList");
-            }
-        }
+        public ObservableCollection<CalendarModel> CalendarListEdit { get; set; }
+        UserModel _user;
         public string Month
         {
             get => _month;
@@ -32,17 +27,35 @@ namespace Workflow.ViewModels
                 OnPropertyChanged("Month");
             }
         }
-        public CalendarEditViewModel(List<CalendarModel> calendar, string month)
+        public CalendarEditViewModel(List<CalendarModel> calendar, string month, INavigation navigation)
         {
             this.Month = month;
-            this.CalendarList = new ObservableCollection<CalendarModel>();
-            foreach (var day in calendar)
+            this.Navigation = navigation;
+            this.CalendarListEdit = new ObservableCollection<CalendarModel>();
+            //this._user = user;
+
+            LoadCalendar = new Command(() =>
             {
-                CalendarList.Add(day);
-            }
+                foreach (var day in calendar)
+                {
+                    CalendarListEdit.Add(day);
+                }
+            });
+            
             InvertDay = new Command<CalendarModel>((day) =>
             {
                 day.Workday = !day.Workday;
+            });
+
+            UpdateCalendar = new Command(async () =>
+            {
+                var calendr = new List<CalendarModel>();
+                foreach (var day in CalendarListEdit)
+                {
+                    calendr.Add(day.Clone() as CalendarModel);
+                }
+                MessagingCenter.Send<CalendarEditViewModel, List<CalendarModel>>(this, "UpdateCalendar", calendr);
+                await Navigation.PopAsync();
             });
         }
     }
