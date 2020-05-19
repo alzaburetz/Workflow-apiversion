79b26d6f14398b92dbb6ee5106787739d0d249de
Anton Motin <anton.motin.dev@gmail.com>
2020-05-19 19:05:53 +0300

    Popup for adding a note to a calendar day added

---
+++ Workflow/Workflow/Views/Popups/NotePopup.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Rg.Plugins.Popup.Pages;
+
+namespace Workflow.Views.Popups
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotePopup : PopupPage
+    {
+        public NotePopup(string date, string note)
+        {
+            InitializeComponent();
+            Date.Text = date;
+            Note.Text = note;
+        }
+    }
+}
