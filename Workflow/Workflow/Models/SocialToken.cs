ca262782a48b1af890e719b52e38cb4628cc4f9e
Anton Motin <anton.motin.dev@gmail.com>
2020-05-20 22:53:29 +0300

    Model for login via google added

---
+++ Workflow/Workflow/Models/SocialToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Workflow.Models
+{
+    [Serializable]
+    public class SocialToken
+    {
+        [JsonProperty("token")]
+        public string Token { get; set; }
+        [JsonProperty("has_logged_in")]
+        public bool Logged { get; set; }
+    }
+}
