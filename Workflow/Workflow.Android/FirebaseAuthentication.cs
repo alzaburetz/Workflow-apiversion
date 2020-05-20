a7db185b6d64a2a088d04ff50c59af2b99d44a15
Anton Motin <anton.motin.dev@gmail.com>
2020-05-20 22:57:28 +0300

    Firebase authentication implemented for android

---
+++ Workflow/Workflow.Android/FirebaseAuthentication.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Workflow.Services;
+using Xamarin.Forms;
+using Workflow.Droid;
+using System.Threading.Tasks;
+using Plugin.CurrentActivity;
+using Android.Gms.Auth.Api;
+using Android.Content.PM;
+using Android.Content.Res;
+
+[assembly:Dependency(typeof(FirebaseAuthentication))]
+namespace Workflow.Droid
+{
+    public class FirebaseAuthentication:IFirebaseAuth
+    {
+        MainActivity main;
+        public async Task<string> Authenticate()
+        {
+            main = (MainActivity)CrossCurrentActivity.Current.Activity;
+            main.SignInClick();
+            return ";";
+        }
+    }
+}
