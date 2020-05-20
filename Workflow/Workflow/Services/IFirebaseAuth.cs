84ed9a70153d3ed1d4cbe21c61c613cb1e585272
Anton Motin <anton.motin.dev@gmail.com>
2020-05-20 22:54:07 +0300

    Interface for firebase authentication added

---
+++ Workflow/Workflow/Services/IFirebaseAuth.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workflow.Services
+{
+    public interface IFirebaseAuth
+    {
+        Task<string> Authenticate();
+    }
+}
