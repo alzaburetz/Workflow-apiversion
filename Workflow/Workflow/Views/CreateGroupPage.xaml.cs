caf063d1fd8deb14889205f4fcf9418a167a1436
Anton Motin <anton.motin.dev@gmail.com>
2020-05-25 22:55:19 +0300

    Second constructor for editig group

--- Workflow/Workflow/Views/CreateGroupPage.xaml.cs
+++ Workflow/Workflow/Views/CreateGroupPage.xaml.cs
@@ -9,6 +9,7 @@ using Xamarin.Forms.Xaml;
 
 using Workflow.ViewModels;
 using Workflow.Models;
+using System.Text.RegularExpressions;
 
 namespace Workflow.Views
 {
@@ -20,12 +21,30 @@ namespace Workflow.Views
         {
             InitializeComponent();
             BindingContext = viewModel = new CreateGroupViewModel(navigation);
+            viewModel.ButtonText = "Создать группу";
+            Submit.Clicked += CreateGroup;
+        }
+
+        public CreateGroupPage(INavigation navigation, GroupModel group)
+        {
+            InitializeComponent();
+            BindingContext = viewModel = new CreateGroupViewModel(navigation);
+            viewModel.Group = group;
+            viewModel.ButtonText = "Редактировать группу";
+            Submit.Clicked += EditGroup;
         }
 
         void CreateGroup(object sender, EventArgs args)
         {
             var group = new GroupModel(GroupName.Text, Description.Text);
             viewModel.CreateGroupCommand.Execute(group);
+        }
+
+        void EditGroup(object sender, EventArgs args)
+        {
+            viewModel.Group.Name = GroupName.Text;
+            viewModel.Group.Description = Description.Text;
+            viewModel.EditGroupCommand.Execute(null);
         }
     }
 }
