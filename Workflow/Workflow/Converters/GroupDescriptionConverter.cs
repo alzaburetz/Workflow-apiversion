09fa5c1d1c4829788ce8d85c2e6fd849b23d92c9
Anton Motin <anton.motin.dev@gmail.com>
2020-06-18 23:39:27 +0300

    Now can format links like (Name)[<link>]

--- Workflow/Workflow/Converters/GroupDescriptionConverter.cs
+++ Workflow/Workflow/Converters/GroupDescriptionConverter.cs
@@ -5,22 +5,23 @@ using System.Globalization;
 using System.Text;
 
 using Xamarin.Forms;
+using System.Text.RegularExpressions;
 
 namespace Workflow.Converters
 {
     public class GroupDescriptionConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object value, Type targetType, object parameter = null, CultureInfo culture = null)
         {
             var words = ((string)value).Split(' ');
             FormattedString result = new FormattedString();
             string textblock = string.Empty;
+            Regex regex = new Regex(@"\(\w+\)\[http://\S+]|\(\w+\)\[https://\S+]");
             for (int i = 0; i < words.Length; i++)
             {
                 if (!words[i].Contains("https://"))
                 {
                     textblock += words[i] + " ";
-
                 }
                 else
                 {
@@ -29,18 +30,45 @@ namespace Workflow.Converters
                         Text = textblock
                     });
                     textblock = string.Empty;
+                    if (regex.IsMatch(words[i]))
+                    {
+                        var a = words[i];
+                        string linkname = a.Substring(1, a.IndexOf(')') - 1);
+                        string link = a.Substring(a.IndexOf('[') + 1, a.IndexOf(']') - a.IndexOf('[') - 1);
+                        var span1 = new Span()
+                        {
+                            Text = linkname,
+                            TextColor = parameter == null ? Color.Blue : Color.Default,
+                            TextDecorations = parameter == null ? TextDecorations.Underline : TextDecorations.None
+                        };
+                        if (parameter == null)
+                        {
+                            TapGestureRecognizer openUri1 = new TapGestureRecognizer()
+                            {
+                                Command = new Command<string>((url) => Device.OpenUri(new Uri(url.Trim()))),
+                                CommandParameter = link
+                            };
+                            span1.GestureRecognizers.Add(openUri1);
+                        }
+                        
+                        result.Spans.Add(span1);
+                        continue;
+                    }
                     var span = new Span()
                     {
                         Text = words[i],
-                        TextColor = Color.Blue,
-                        TextDecorations = TextDecorations.Underline
+                        TextColor = parameter == null ? Color.Blue : Color.Default,
+                        TextDecorations = parameter == null ? TextDecorations.Underline : TextDecorations.None
                     };
-                    TapGestureRecognizer openUri = new TapGestureRecognizer()
+                    if (parameter == null)
                     {
-                        Command = new Command<string>((url) => Device.OpenUri(new Uri(url.Trim()))),
-                        CommandParameter = span.Text
-                    };
-                    span.GestureRecognizers.Add(openUri);
+                        TapGestureRecognizer openUri = new TapGestureRecognizer()
+                        {
+                            Command = new Command<string>((url) => Device.OpenUri(new Uri(url.Trim()))),
+                            CommandParameter = span.Text
+                        };
+                        span.GestureRecognizers.Add(openUri);
+                    }
                     result.Spans.Add(span);
                 }
             }
