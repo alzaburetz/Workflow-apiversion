20c201c7f2bd39b3567ef505a7bf44ec52215868
Anton Motin <anton.motin.dev@gmail.com>
2020-06-18 23:43:20 +0300

    Interface to implement VK login added

---
+++ Workflow/Workflow/Services/IVKLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Workflow.Models;
+
+namespace Workflow.Services
+{
+    public interface IVKLogin
+    {
+        Task<UserAuthModel> LoginViaVK();
+    }
+}
