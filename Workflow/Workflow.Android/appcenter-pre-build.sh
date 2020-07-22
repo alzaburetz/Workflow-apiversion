f3fe6d6faf27592f1ab778b1aac08688071c7c62
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 18:27:23 +0300

    Back to double quotes

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
-# ACTIVITY=MainActivity.cs
-MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
+#ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
+ACTIVITY=MainActivity.cs
+# MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
 LABEL_VAR=${LABEL}
 PACKAGE=${PACKAGE_NAME}
-# MANIFEST=Properties/AndroidManifest.xml
-sed -i 's/Label = "[-a-zA-Z0-9_ ]*"/Label = "${LABEL_VAR}"/' ${ACTIVITY} 
-sed -i 's/package="[a-z | .]*\"/package="${PACKAGE}"/' ${MANIFEST}
+MANIFEST=Properties/AndroidManifest.xml
+ICON=Resources/drawable
+
+sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL_VAR}\"/" ${ACTIVITY};
+sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
