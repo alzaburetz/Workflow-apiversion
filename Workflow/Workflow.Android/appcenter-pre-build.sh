b992ff5db3b05e4656c20c44b5746f443a0e9c29
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 13:56:59 +0300

    Label change

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,3 +1,5 @@
 ﻿#!/usr/bin/env bash
 ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs;
+APP_DISPLAY_NAME=${TEST}
+sed -i '' "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${APP_DISPLAY_NAME}\"/" ${ACTIVITY}
 cat ${ACTIVITY}
