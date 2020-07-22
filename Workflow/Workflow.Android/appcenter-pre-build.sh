b652d06f12743efa7ccd1b6153c8719a5905df38
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 15:31:21 +0300

    Script fix for production

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/MainActivity.cs
+# ACTIVITY=MainActivity.cs
 MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/Properties/AndroidManifest.xml
-
-sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL}\"/" ${ACTIVITY}
+# MANIFEST=Properties/AndroidManifest.xml
+sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL}\"/" ${ACTIVITY} |
 sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE_NAME}\"/" ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
