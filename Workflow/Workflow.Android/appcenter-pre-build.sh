08518d45335dac65e7b084b5d34034356381d429
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 16:08:07 +0300

    small  fix

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -3,6 +3,6 @@ ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/MainActivity.cs
 # ACTIVITY=MainActivity.cs
 MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/Properties/AndroidManifest.xml
 # MANIFEST=Properties/AndroidManifest.xml
-sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL}\"/" ${ACTIVITY} |
+sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL}\"/" ${ACTIVITY} 
 # sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE_NAME}\"/" ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
