2b6e189773ce9e5dab22156b8735ee8627c39708
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 19:13:44 +0300

    Prod paths

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
-ACTIVITY=MainActivity.cs
-# MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
+ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
+# ACTIVITY=MainActivity.cs
+MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
 LABEL_VAR=${LABEL}
 PACKAGE=${PACKAGE_NAME}
-MANIFEST=Properties/AndroidManifest.xml
+# MANIFEST=Properties/AndroidManifest.xml
 ICON=Resources/drawable
 
 sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL_VAR}\"/" ${ACTIVITY};
