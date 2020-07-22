a7986953d689ef557b622057d97320ccebdef332
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 19:17:44 +0300

    remove of variable

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -7,6 +7,6 @@ PACKAGE=${PACKAGE_NAME}
 # MANIFEST=Properties/AndroidManifest.xml
 ICON=Resources/drawable
 
-sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL_VAR}\"/" ${ACTIVITY}
+sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"testlabel\"/" ${ACTIVITY}
 # sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
