488b216c31984a6f3318e79128707bc84d28a4e7
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 16:42:08 +0300

    Single quotes instead of double quotes

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -5,6 +5,6 @@ MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/Andr
 LABEL_VAR=${LABEL}
 PACKAGE=${PACKAGE_NAME}
 # MANIFEST=Properties/AndroidManifest.xml
-sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL_VAR}\"/" ${ACTIVITY} 
-sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
+sed -i 's/Label = "[-a-zA-Z0-9_ ]*"/Label = "${LABEL_VAR}"/' ${ACTIVITY} 
+sed -i 's/package="[a-z | .]*\"/package="${PACKAGE}"/' ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
