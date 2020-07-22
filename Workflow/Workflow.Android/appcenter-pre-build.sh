4a4099f230626035a73835067ca5047fe47a6763
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 13:16:36 +0300

    fix not working script

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,11 +1,7 @@
-﻿#!/usr/bin/env bash
-
+﻿#!/usr/env/bin bash
 APP_DISPLAY_NAME="test"
 ANDROID_MAINACTIVITY_FILE=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/MainActivity.cs
-
 echo "##[section][Pre-Build Action] Variable from env TEST = ${TEST}"
-
 sed -i '' "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${APP_DISPLAY_NAME}\"/" ${ANDROID_MAINACTIVITY_FILE}
-
     echo "##[section][Pre-Build Action] - MainActivity.cs File content:"
     cat ${ANDROID_MAINACTIVITY_FILE}
