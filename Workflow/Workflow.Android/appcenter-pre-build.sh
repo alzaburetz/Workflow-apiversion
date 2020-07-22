7b31573705fc1f61277bf74438a9db34d8ee17c2
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 13:36:43 +0300

    rewrite appcenter script

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,8 +1,3 @@
 ﻿#!/usr/bin/env bash
-PACKAGE_NAME=${PACKAGE}
-ANDROID_MAINACTIVITY_FILE=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
-APP_DISPLAY_NAME="AAAAAAAAAAA"
-echo "##[section][Pre-Build Action] Variable from env TEST = ${TEST}"
-sed -i '' "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${APP_DISPLAY_NAME}\"/" ${ANDROID_MAINACTIVITY_FILE}
-    echo "##[section][Pre-Build Action] - MainActivity.cs File content:"
-    cat ${ANDROID_MAINACTIVITY_FILE}
+ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
+cat ${ACTIVITY}
