76d11e43d7676d3446b7dc902ca9c6494f01d8dc
Anton Motin <anton.motin.dev@gmail.com>
2020-07-22 16:25:29 +0300

    new vars

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -2,7 +2,9 @@
 ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/MainActivity.cs
 # ACTIVITY=MainActivity.cs
 MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Droid/Properties/AndroidManifest.xml
+LABEL_VAR=${LABEL}
+PACKAGE=${PACKAGE_NAME}
 # MANIFEST=Properties/AndroidManifest.xml
-sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL}\"/" ${ACTIVITY} 
-# sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE_NAME}\"/" ${MANIFEST}
+sed -i "s/Label = \"[-a-zA-Z0-9_ ]*\"/Label = \"${LABEL_VAR}\"/" ${ACTIVITY} 
+sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
 # sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
