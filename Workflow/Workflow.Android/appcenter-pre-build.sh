7d326e09ae71b924dd9bdf5bba0fa1b91f405a34
Anton Motin <anton.motin.dev@gmail.com>
2020-07-24 15:09:53 +0300

    Final test

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
+ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
 ACTIVITY=MainActivity.cs
-# MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
+MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
 LABEL_VAR=${LABEL}
 PACKAGE=${PACKAGE_NAME}
 MANIFEST=Properties/AndroidManifest.xml
@@ -9,8 +9,11 @@ ICON=Resources/drawable
 
 sed -i.bak "s/Label = \"[А-Яа-я|' ']*\"/Label = \"${LABEL_VAR}\"/"  $ACTIVITY
 rm -f ${ACTIVITY}.bak
-echo ${LABEL_VAR}
+
 cat ${ACTIVITY}
-exit 1;
-# sed -i "s/package=\"[a-z | .]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
-# sed -i "s/package=\"[-a-z]*\"/package=\"ru.meme.meme\"" ${MANIFEST}
+echo
+sed -i.bak "s/package=\"[a-z | . | _ ]*\"/package=\"${PACKAGE}\"/" ${MANIFEST}
+rm -f ${MANIFEST}.bak
+sed -i.bak "s/label=\"[-a-zA-ZА-Яа-я | ' ']*\"/label=\"${LABEL_VAR}\"/" ${MANIFEST}
+rm -f ${MANIFEST}.bak
+cat ${MANIFEST}
