0f7476586ef7c6e522a605757c57596af007adc7
Anton Motin <anton.motin.dev@gmail.com>
2020-07-24 15:10:30 +0300

    final test II

--- Workflow/Workflow.Android/appcenter-pre-build.sh
+++ Workflow/Workflow.Android/appcenter-pre-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 ACTIVITY=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/MainActivity.cs
-ACTIVITY=MainActivity.cs
+# ACTIVITY=MainActivity.cs
 MANIFEST=${APPCENTER_SOURCE_DIRECTORY}/Workflow/Workflow.Android/Properties/AndroidManifest.xml
 LABEL_VAR=${LABEL}
 PACKAGE=${PACKAGE_NAME}
-MANIFEST=Properties/AndroidManifest.xml
+# MANIFEST=Properties/AndroidManifest.xml
 ICON=Resources/drawable
 
 sed -i.bak "s/Label = \"[А-Яа-я|' ']*\"/Label = \"${LABEL_VAR}\"/"  $ACTIVITY
